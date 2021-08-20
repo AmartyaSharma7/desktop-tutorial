1405d2063a7a5281ede25d391690b9547d53eab8
AmartyaSharma7 <amartyabsharma@gmail.com>
2021-08-20 14:43:51 +0530

    ok

--- dp.cpp
+++ dp.cpp
@@ -254,3 +254,20 @@ int LongestRepeatingSubsequence(string str){
             res = max(res, dp[i][0]);
         return res;
     }
+
+//https://practice.geeksforgeeks.org/problems/friends-pairing-problem5425/1#
+//Friends pairing
+
+int countFriendsPairings(int n) 
+    { 
+        // code here
+        long long int dp[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            if(i<=2)
+                dp[i]=i;
+            else
+              dp[i]=(dp[i-1]%1000000007 + (i-1)%1000000007 * dp[i-2]%1000000007 )%1000000007;
+        }
+        return dp[n];
+    }
