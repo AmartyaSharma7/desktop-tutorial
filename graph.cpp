2481412454c202d8ede5ea983a26ef1b3ca00f47
AmartyaSharma7 <amartyabsharma@gmail.com>
2021-10-03 10:34:55 +0530

    dfs

--- graph.cpp
+++ graph.cpp
@@ -23,4 +23,31 @@
 	    
 	    return bfs;
 	}
+
+
+//dfs
+ void dfsrec(vector<int> adj[],int s,bool visited[],vector<int> &res){
+    
+    visited[s]=true;
+    res.push_back(s);
+    for(int u : adj[s]){
+        if(visited[u] == false){
+            dfsrec(adj,u,visited,res);
+        }
+    }
+    
+}
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> res;
+        bool visited[V];
+        for(int i=0;i<V;i++){
+            visited[i]=false;
+        }
+        for(int i=0;i<V;i++){
+            if(visited[i]==false)
+              dfsrec(adj,i,visited,res);
+        }
+        return res;
+    }
     
